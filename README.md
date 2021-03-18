69f5c7a809652b912b577e825ed3222702ade756
krsuhjunho <krsuhjunho@gmail.com>
2021-03-18 05:26:55 +0000

    2021-03-18 Change docker-run.sh

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ Postgresql 9.2.24 Base
 Denbun POP版 V3.3P R8.0
 
 https://www.denbun.com/ja/pop/index.html
+
+
+# git pull https://github.com/krsuhjunho/centos7-base-denbunmail.git
+# ./docker-run.sh
+Change Port && Hostname && Container Name
+
+
+
+
--- docker-run.sh
+++ docker-run.sh
@@ -3,8 +3,14 @@
 DOCKER_CONTAINER_NAME="denbun-test"
 CONTAINER_HOST_NAME="denbun-test"
 SSH_PORT=22456
-HTTP_PORT=8081
+HTTP_PORT=8011
 DENBUN_BASE_IMAGE_NAME="wnwnsgh/centos7-base-denbun"
+SERVER_IP=$(curl -s ifconfig.me)
+ADMIN_URL="cgi-bin/dnpwml/dnpwmlconfig.cgi?"
+USER_URL="cgi-bin/dnpwml/dnpwmljs.cgi"
+HTTP_BASE="http://"
+
+
 
 docker rm -f ${DOCKER_CONTAINER_NAME}
 
@@ -15,4 +21,14 @@ docker run -tid --privileged=true \
 -p ${SSH_PORT}:22 -p ${HTTP_PORT}:80 \
 ${DENBUN_BASE_IMAGE_NAME}
 
-docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash
+
+#docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash
+
+
+echo ""
+echo "Admin URL => ${HTTP_BASE}${SERVER_IP}:${HTTP_PORT}/${ADMIN_URL}"
+echo ""
+echo "User  URL => ${HTTP_BASE}${SERVER_IP}:${HTTP_PORT}/${USER_URL}"
+echo ""
+
+

7ff6febba1ddd9461211a1ab3a2e3b05e577e40c
krsuhjunho <krsuhjunho@gmail.com>
2021-03-19 16:08:57 +0000

    2021-03-19 update only docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -11,7 +11,27 @@ ADMIN_URL="cgi-bin/dnpwml/dnpwmlconfig.cgi?"
 USER_URL="cgi-bin/dnpwml/dnpwmljs.cgi"
 HTTP_BASE="http://"
 TIME_ZONE="Asia/Tokyo"
+TODAY=$(date "+%Y-%m-%d")
+COMMIT_COMMENT="$2"
+BUILD_OPTION="$1"
 
+DOCKER_IMAGE_BUILD()
+{
+docker build -t ${BASE_IMAGE_NAME} .
+}
+
+DOCKER_IMAGE_PUSH()
+{
+docker push ${BASE_IMAGE_NAME}
+}
+
+GIT_COMMIT_PUSH()
+{
+git add . --ignore-removal
+git commit -m "${TODAY} ${COMMIT_COMMENT}"
+git config credential.helper store
+git push origin main
+}
 
 DOCKER_CONTAINER_REMOVE()
 {
@@ -30,7 +50,6 @@ docker run -tid --privileged=true \
 ${BASE_IMAGE_NAME}
 }
 
-
 DOCKER_CONTAINER_BASH()
 {
 docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash
@@ -47,6 +66,13 @@ echo ""
 
 MAIN()
 {
+
+if [ "$BUILD_OPTION" == "--build" ]; then
+    DOCKER_IMAGE_BUILD
+	DOCKER_IMAGE_PUSH
+	GIT_COMMIT_PUSH
+fi
+
 DOCKER_CONTAINER_REMOVE
 DOCKER_CONTAINER_CREATE
 DOCKER_CONTAINER_BASH
